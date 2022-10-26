6d443f55a3cfec829ad7f4e048bb4680136c247f
anurag-4508 <anuragmandloi30@gmail.com>
2022-10-26 16:01:23 +0530

    Updated Donate,News Pages

--- payment/index.js
+++ payment/index.js
@@ -132,3 +132,21 @@ app.post("/paynow", [parseUrl, parseJson], (req, res) => {
 app.listen(PORT, () => {
   console.log(`App is listening on Port ${PORT}`);
 });
+
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 20) {
+      $('#toTopBtn').fadeIn();
+    } else {
+      $('#toTopBtn').fadeOut();
+    }
+  });
+
+  $('#toTopBtn').click(function() {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+  });
+});
